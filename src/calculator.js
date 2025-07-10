3d68b009c5aa155b6d21ed33dda02ad8d66e027a
Harish Saini <harishsingodiya@gmail.com>
2025-07-10 11:29:02 +0530

    feat: throw error listing all negative numbers

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,18 @@
 function add(numbers) {
-    if (numbers === "") return 0;
+  if (!numbers) return 0;
+
+  let delimiter = /,|\n/; // default delimiters: comma or newline
+
+  const numArray = numbers.split(delimiter).map(Number);
+
+  const negativeNumbers = numArray.filter((n) => n < 0);
+  if (negativeNumbers.length > 0) {
+    throw new Error(
+      `negative numbers not allowed ${negativeNumbers.join(",")}`
+    );
   }
-  
-  module.exports = add;
+
+  return numArray.reduce((sum, num) => sum + num, 0);
+}
+
+module.exports = add;
