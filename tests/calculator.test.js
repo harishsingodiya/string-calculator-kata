f61db4f0821535caacb268b0911d1de004e2b6ea
Harish Saini <harishsingodiya@gmail.com>
2025-07-10 11:46:40 +0530

    test: returns number if only one number

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -5,6 +5,10 @@ describe("String Calculator", () => {
     expect(add("")).toBe(0);
   });
 
+  test('returns number if only one number', () => {
+    expect(add("5")).toBe(5);
+  });
+
   test("throws error for negative numbers", () => {
     expect(() => add("1,-2,-5")).toThrow("negative numbers not allowed -2,-5");
   });
